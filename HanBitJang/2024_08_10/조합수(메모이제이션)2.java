200a06174c8b4f1d159ddd25176e226e138338e9
biiit4894 <ghiesf@naver.com>
2024-08-10 23:30:32 +0900

    [ETC] 조합수(메모이제이션) / 알고리즘 문제풀이 입문(DFS)

---
+++ HanBitJang/2024_08_10/조합수(메모이제이션)2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+// 메모이제이션 사용 O
+
+public class Main {
+    int[][] dy = new int[35][35];
+
+    public int DFS(int n, int r) {
+        if (dy[n][r] > 0) {
+            return dy[n][r];
+        }
+        if (n == r || r == 0) {
+            return 1;
+        } else {
+            return dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int r = kb.nextInt();
+        System.out.println(T.DFS(n, r));
+    }
+}
