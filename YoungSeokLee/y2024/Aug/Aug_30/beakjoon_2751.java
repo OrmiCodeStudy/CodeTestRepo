ab999e5fc33068d5f42ff340d4aabe015db0961f
BlueHarmel <dldjdtjr@naver.com>
2024-08-31 21:49:01 +0900

    [BOJ] 수 정렬하기2 (2751) / 실버5

--- YoungSeokLee/y2024/Aug/Aug_30/beakjoon_2751.java
+++ YoungSeokLee/y2024/Aug/Aug_30/beakjoon_2751.java
@@ -1,2 +1,27 @@
-package y2024.Aug.Aug_30;public class beakjoon_2751 {
+package y2024.Aug.Aug_30;
+
+import java.io.*;
+
+public class beakjoon_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] numArr = new int[2000001];
+        for(int i = 0;i<n;i++){
+            int num = Integer.parseInt(br.readLine());
+            numArr[num+1000000]=1;
+        }
+
+        for(int i =0;i<2000001;i++){
+            if(numArr[i]==1){
+                bw.write(i-1000000+"");
+                bw.newLine();
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
