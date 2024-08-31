77eb5547eba4da54b1d552a3f22bee372242f691
BlueHarmel <dldjdtjr@naver.com>
2024-08-31 21:39:04 +0900

    [BOJ] 수 정렬하기3(10989)/브론즈11

--- YoungSeokLee/y2024/Aug/Aug_28/beakjoon_10989.java
+++ YoungSeokLee/y2024/Aug/Aug_28/beakjoon_10989.java
@@ -1,2 +1,34 @@
-package y2024.Aug.Aug_28;public class beakjoon_10989 {
+package y2024.Aug.Aug_28;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class beakjoon_10989 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] nArr = new int[10001];
+
+        for(int i =0;i<n;i++){
+            nArr[Integer.parseInt(br.readLine())] += 1;
+        }
+
+        for(int i =0;i<nArr.length;i++){
+            if(nArr[i]!=0){
+                for(int j =0;j<nArr[i];j++){
+                    bw.write(i+"");
+                    bw.newLine();
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
