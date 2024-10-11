4b6c65321380d061bcecb19d949840d4275d6d88
BlueHarmel <dldjdtjr@naver.com>
2024-10-11 13:17:54 +0900

    [BOJ] 이중 우선순위 큐(7662)-TreeMap 골드4

--- YoungSeokLee/y2024/Oct/Oct_11/beakjoon_7662.java
+++ YoungSeokLee/y2024/Oct/Oct_11/beakjoon_7662.java
@@ -1,76 +1,43 @@
 package y2024.Oct.Oct_11;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/*
-이중 우선순위 큐 - 골드 4
-
-q : 정수를 저장하는 이중 우선순위 큐
-q에 저장된 각 정수의 값 : 우선순위
-
-최종 연산 후 q에 저장된 데이터 중 최댓값과 최솟값을 출력하라
-
-T : 테스트 데이터 수
-k : q에 적용할 연산의 개수
-D n
-    - n == -1 : 최댓값을 삭제
-    - n == 1 : 최솟값을 삭제
-I n: 정수 n을 q에 삽입
-
-q가 비어있는데 d가 입력되면 무시
-최종결과가 비어있다면 EMPTY 출력
- */
 public class beakjoon_7662 {
-
-    void insertSort(ArrayList<Integer> q, int newElement){
-        int i;
-        for(i=0;i<q.size()&&q.get(i)<newElement;i++);
-        q.add(i, newElement);
-    }
-
-    void delete(ArrayList<Integer> q, int choice){
-        if(q.isEmpty())
-            return;
-
-        if(choice == -1){
-            q.remove(0);
-        } else {
-            q.remove(q.size()-1);
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int t = Integer.parseInt(br.readLine());
-        StringTokenizer stk;
 
-        ArrayList<Integer> q = new ArrayList<>();
-        beakjoon_7662 T = new beakjoon_7662();
-
-        for(int i =0;i<t;i++){
+        for (int i = 0; i < t; i++) {
+            TreeMap<Integer, Integer> map = new TreeMap<>();
             int k = Integer.parseInt(br.readLine());
-            for(int l=0;l<k;l++){
-                stk = new StringTokenizer(br.readLine());
-                char command = stk.nextToken().charAt(0);
-                int n = Integer.parseInt(stk.nextToken());
 
-                if(command =='I')
-                    T.insertSort(q,n);
-                else
-                    T.delete(q,n);
+            for (int j = 0; j < k; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                char command = st.nextToken().charAt(0);
+                int n = Integer.parseInt(st.nextToken());
+
+                if (command == 'I') {
+                    map.put(n, map.getOrDefault(n, 0) + 1);
+                } else if (!map.isEmpty()) {
+                    int key = n == 1 ? map.lastKey() : map.firstKey();
+                    if (map.get(key) == 1) {
+                        map.remove(key);
+                    } else {
+                        map.put(key, map.get(key) - 1);
+                    }
+                }
             }
-            if(!q.isEmpty())
-                bw.write(q.get(q.size()-1)+" "+q.get(0)+"\n");
-            else
+
+            if (map.isEmpty()) {
                 bw.write("EMPTY\n");
+            } else {
+                bw.write(map.lastKey() + " " + map.firstKey() + "\n");
+            }
         }
+
         bw.flush();
         bw.close();
         br.close();
